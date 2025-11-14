00abf42acad0af3169125bfef971a706ddf6cc26
PaulineFss <pauline.fassy@outlook.fr>
2025-11-14 15:29:14 +0100

    Ajout tout ce qui est rapport avec la classe Collection

---
+++ library-boot/src/main/java/library_boot/service/CollectionService.java
@@ -0,0 +1,54 @@
+package library_boot.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import library_boot.dao.IDAOCollection;
+import library_boot.model.Collection;
+
+public class CollectionService {
+
+	
+	@Autowired
+	IDAOCollection daoCollection;
+
+	public Collection getById(Integer id) throws RuntimeException
+	{
+		if(id==null) 
+		{
+			throw new RuntimeException("L'id d'une collection ne peut pas etre null");	
+		}
+		Optional <Collection> opt = daoCollection.findById(id);
+		if(opt.isEmpty()) {return null;}
+		else {return opt.get();}
+	}
+
+	public List<Collection> getAll()
+	{
+		return daoCollection.findAll();
+	}
+	
+	public Collection create(Collection collection) 
+	{
+		return daoCollection.save(collection);
+	}
+
+	public Collection update(Collection collection) 
+	{
+		return daoCollection.save(collection);
+	}
+
+	public void deleteById(Integer id) 
+	{
+		daoCollection.deleteById(id);
+	}
+
+	public void delete(Collection collection)
+	{
+		daoCollection.delete(collection);
+	}
+	
+}
+
