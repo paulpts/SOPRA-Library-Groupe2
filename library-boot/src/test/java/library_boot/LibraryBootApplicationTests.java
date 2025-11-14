8f845e2dc4f4130dbcdc35652fdcfeb082af0404
paulpts <p.pontois@outlook.com>
2025-11-14 12:27:03 +0100

    first commit

---
+++ library-boot/src/test/java/library_boot/LibraryBootApplicationTests.java
@@ -0,0 +1,13 @@
+package library_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
