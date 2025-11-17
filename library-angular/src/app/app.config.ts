9aab0ca30e5a58fe53ab4feb183bd766ed6d79c5
paulpts <p.pontois@outlook.com>
2025-11-17 15:22:49 +0100

    corrections mise en commun

--- library-angular/src/app/app.config.ts
+++ library-angular/src/app/app.config.ts
@@ -1,7 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideHttpClient} from '@angular/common/http';
 
